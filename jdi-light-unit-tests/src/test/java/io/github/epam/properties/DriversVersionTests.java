09e89940a1c33a67731df904db93f906ea59cfd4
Roman Iovlev <Roman_Iovlev@epam.com>
2020-01-19 14:20:52 +0300

    add module with tests

---
+++ jdi-light-unit-tests/src/test/java/io/github/epam/properties/DriversVersionTests.java
@@ -0,0 +1,45 @@
+package io.github.epam.properties;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.security.InvalidParameterException;
+import java.util.Properties;
+
+import static io.github.epam.Property.*;
+import static io.github.epam.PropertyValidationUtils.*;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.fail;
+
+public class DriversVersionTests {
+
+    @DataProvider
+    public static Object[] negativeData() {
+        return new Object[]{"latest", "LATES", "prelatest", "", "RELATEST", "2,31", "ten.eleven", "1.76", "2"};
+    }
+
+    @DataProvider
+    public static Object[] positiveData() {
+        return new Object[]{"LATEST", "PRELATEST", "1.23.3", "3.141.59", "4.0.0-alpha-2"};
+    }
+
+    @Test(dataProvider = "negativeData")
+    public void negativeTest(String value) {
+        Properties properties = new Properties();
+        properties.setProperty(DRIVERS_VERSION_PROPERTY.getName(), value);
+        try {
+            validateProperties(properties);
+            fail("Value '" + value + "' should not be valid for this test.");
+        } catch (InvalidParameterException exp) {
+            String expMessage = exp.getMessage();
+            assertEquals(expMessage, DRIVERS_VERSION_PROPERTY.getExMsg() + LINK_TO_EXAMPLES);
+        }
+    }
+
+    @Test(dataProvider = "positiveData")
+    public void positiveTest(String value) {
+        Properties properties = new Properties();
+        properties.setProperty(DRIVERS_VERSION_PROPERTY.getName(), value);
+        validateProperties(properties);
+    }
+}
