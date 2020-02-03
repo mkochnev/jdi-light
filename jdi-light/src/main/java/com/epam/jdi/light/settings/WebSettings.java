699716096948836f7dc9db8a682a1a5132ebc67f
Dmitrii Savin <Dmitrii_Savin@epam.com>
2020-02-03 17:15:12 +0300

    android adapt (easy)

--- jdi-light/src/main/java/com/epam/jdi/light/settings/WebSettings.java
+++ jdi-light/src/main/java/com/epam/jdi/light/settings/WebSettings.java
@@ -1,5 +1,6 @@
 package com.epam.jdi.light.settings;
 
+import com.epam.jdi.light.asserts.core.SoftAssert;
 import com.epam.jdi.light.common.*;
 import com.epam.jdi.light.driver.WebDriverFactory;
 import com.epam.jdi.light.driver.get.DriverTypes;
@@ -14,6 +15,7 @@ import com.epam.jdi.tools.func.JFunc;
 import com.epam.jdi.tools.func.JFunc1;
 import io.appium.java_client.AppiumDriver;
 import org.openqa.selenium.PageLoadStrategy;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 import java.io.File;
@@ -27,25 +29,33 @@ import java.util.Properties;
 import static com.epam.jdi.light.common.ElementArea.CENTER;
 import static com.epam.jdi.light.common.ElementArea.SMART_CLICK;
 import static com.epam.jdi.light.common.Exceptions.exception;
+import static com.epam.jdi.light.common.PageChecks.parse;
 import static com.epam.jdi.light.common.SetTextTypes.SET_TEXT;
 import static com.epam.jdi.light.common.TextTypes.SMART_TEXT;
-import static com.epam.jdi.light.driver.get.DriverData.CAPABILITIES_FOR_MOBILE;
-import static com.epam.jdi.light.driver.get.DriverData.COMMON_CAPABILITIES;
+import static com.epam.jdi.light.driver.ScreenshotMaker.SCREENSHOT_STRATEGY;
+import static com.epam.jdi.light.driver.ScreenshotMaker.SCREEN_PATH;
+import static com.epam.jdi.light.driver.WebDriverFactory.INIT_THREAD_ID;
+import static com.epam.jdi.light.driver.get.DriverData.*;
 import static com.epam.jdi.light.driver.get.RemoteDriver.*;
 import static com.epam.jdi.light.driver.sauce.SauceSettings.sauceCapabilities;
+import static com.epam.jdi.light.elements.composite.WebPage.CHECK_AFTER_OPEN;
 import static com.epam.jdi.light.elements.init.PageFactory.preInit;
 import static com.epam.jdi.light.elements.init.UIFactory.$;
 import static com.epam.jdi.light.logger.AllureLoggerHelper.AttachmentStrategy;
+import static com.epam.jdi.light.logger.AllureLoggerHelper.HTML_CODE_LOGGING;
 import static com.epam.jdi.light.logger.JDILogger.instance;
+import static com.epam.jdi.light.logger.LogLevels.parseLogLevel;
 import static com.epam.jdi.light.settings.TimeoutSettings.PAGE_TIMEOUT;
 import static com.epam.jdi.light.settings.TimeoutSettings.TIMEOUT;
 import static com.epam.jdi.tools.EnumUtils.getAllEnumValues;
+import static com.epam.jdi.tools.LinqUtils.filter;
 import static com.epam.jdi.tools.LinqUtils.first;
 import static com.epam.jdi.tools.PathUtils.mergePath;
 import static com.epam.jdi.tools.PropertyReader.fillAction;
 import static com.epam.jdi.tools.PropertyReader.getProperty;
 import static java.lang.Integer.parseInt;
 import static java.util.Arrays.asList;
+import static org.apache.commons.lang3.StringUtils.isNotBlank;
 import static org.apache.commons.lang3.StringUtils.isNotEmpty;
 import static org.openqa.selenium.PageLoadStrategy.*;
 
@@ -137,13 +147,87 @@ public class WebSettings {
         getProperties(TEST_PROPERTIES_PATH);
         fillAction(p -> TIMEOUT = new Timeout(parseInt(p)), "timeout.wait.element");
         fillAction(p -> PAGE_TIMEOUT = new Timeout(parseInt(p)), "timeout.wait.page");
-//
+        fillAction(p -> setDomain(p), "domain");
+        if (DRIVER_NAME.equals(DEFAULT_DRIVER))
+            fillAction(p -> DRIVER_NAME = p, "driver");
+        fillAction(p -> DRIVER_VERSION = p.equalsIgnoreCase(LATEST_VERSION)
+                ? LATEST_VERSION : (p.equalsIgnoreCase(PRELATEST_VERSION))
+                    ? PRELATEST_VERSION : p, "driver.version");
+        fillAction(p -> DRIVERS_FOLDER = p, "drivers.folder");
+        fillAction(p -> SCREEN_PATH = p, "screens.folder");
+        fillAction(p -> SCREENSHOT_STRATEGY = getAttachmentStrategy(p), "screenshot.strategy");
+        fillAction(p -> HTML_CODE_LOGGING = getAttachmentStrategy(p), "html.code.logging");
+//        fillAction(p -> KILL_BROWSER = p, "browser.kill");
+        fillAction(WebSettings::setSearchStrategy, "element.search.strategy");
+        fillAction(p -> BROWSER_SIZE = p, "browser.size");
+        fillAction(p -> PAGE_LOAD_STRATEGY = getPageLoadStrategy(p), "page.load.strategy");
+        fillAction(p -> CHECK_AFTER_OPEN = parse(p), "page.check.after.open");
+        fillAction(SoftAssert::setAssertType, "assert.type");
+        fillAction(p -> TEXT_TYPE = getTextType(p), "text.type");
+        fillAction(p -> SET_TEXT_TYPE = getSetTextType(p), "set.text.type");
+
+        // RemoteWebDriver properties
+        fillAction(p -> DRIVER_REMOTE_URL = getRemoteUrl(p), "remote.type");
+        fillAction(p -> DRIVER_REMOTE_URL = p, "driver.remote.url");
+        fillAction(p -> logger.setLogLevel(parseLogLevel(p)), "log.level");
+        fillAction(p -> SMART_SEARCH_LOCATORS =
+            filter(p.split(";"), l -> isNotBlank(l)), "smart.locators");
+        fillAction(p -> SMART_SEARCH_NAME = getSmartSearchFunc(p), "smart.locators.toName");
+        fillAction(p -> COMMON_CAPABILITIES.put("headless", p), "headless");
+
+        loadCapabilities("chrome.capabilities.path",
+            p -> p.forEach((key,value) -> CAPABILITIES_FOR_CHROME.put(key.toString(),value.toString())));
+        loadCapabilities("ff.capabilities.path",
+            p -> p.forEach((key,value) -> CAPABILITIES_FOR_FF.put(key.toString(),value.toString())));
+        loadCapabilities("ie.capabilities.path",
+            p -> p.forEach((key,value) -> CAPABILITIES_FOR_IE.put(key.toString(),value.toString())));
+        loadCapabilities("edge.capabilities.path",
+            p -> p.forEach((key,value) -> CAPABILITIES_FOR_EDGE.put(key.toString(),value.toString())));
+        loadCapabilities("opera.capabilities.path",
+            p -> p.forEach((key,value) -> CAPABILITIES_FOR_OPERA.put(key.toString(),value.toString())));
         loadCapabilities("appium.capabilities.path",
                 p -> p.forEach((key, value) -> CAPABILITIES_FOR_MOBILE.put(key.toString(), value.toString())));
 
-//        INIT_THREAD_ID = Thread.currentThread().getId();
-//        if (SMART_SEARCH_LOCATORS.size() == 0)
-//            SMART_SEARCH_LOCATORS.add("#%s"/*, "[ui=%s]", "[qa=%s]", "[name=%s]"*/);
+        INIT_THREAD_ID = Thread.currentThread().getId();
+        if (SMART_SEARCH_LOCATORS.size() == 0)
+            SMART_SEARCH_LOCATORS.add("#%s"/*, "[ui=%s]", "[qa=%s]", "[name=%s]"*/);
+    }
+    private static TextTypes getTextType(String type) {
+        TextTypes textType = first(getAllEnumValues(TextTypes.class),
+            t -> t.toString().equals(type));
+        return textType != null
+            ? textType : SMART_TEXT;
+    }
+    private static SetTextTypes getSetTextType(String type) {
+        SetTextTypes textType = first(getAllEnumValues(SetTextTypes.class),
+                t -> t.toString().equals(type));
+        return textType != null
+                ? textType : SET_TEXT;
+    }
+    private static String getRemoteUrl(String prop) {
+        switch (prop.toLowerCase().replaceAll(" ", "")) {
+            case "sauce":
+            case "saucelabs":
+                COMMON_CAPABILITIES = sauceCapabilities();
+                return sauceLabs();
+            case "browserstack": return browserstack();
+            default: return seleniumLocalhost();
+        }
+    }
+    private static JFunc1<String, String> getSmartSearchFunc(String name) {
+        switch (name) {
+            case "camelCase":
+                return StringUtils::toCamelCase;
+            case "snake_case":
+                return StringUtils::toSnakeCase;
+            case "kebab-case":
+                return StringUtils::toKebabCase;
+            case "PascalCase":
+                return StringUtils::toPascalCase;
+            case "UPPER_SNAKE_CASE":
+                return StringUtils::toUpperSnakeCase;
+            default: return StringUtils::toKebabCase;
+        }
     }
 
 
