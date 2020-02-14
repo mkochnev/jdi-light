f24cc098c877177f604b8cfc3869cb6808ed81ec
ekaterinaKronos <ekaterina.larina@kronos.com>
2020-02-14 11:43:51 +0300

    UIElement tests, 2 pull request

--- jdi-light/src/main/java/com/epam/jdi/light/elements/common/UIElement.java
+++ jdi-light/src/main/java/com/epam/jdi/light/elements/common/UIElement.java
@@ -401,7 +401,7 @@ public class UIElement extends JDIBase
         return !displayed();
     }
 
-    @JDIAction(value = "Check that '{name}' is hidden", timeout = 0)
+    @JDIAction(value = "Check that '{name}' exists", timeout = 0)
     public boolean isExist() {
         return noWait(() -> {
             try {
@@ -409,7 +409,7 @@ public class UIElement extends JDIBase
             } catch (Exception ignore) { return false; }
         });
     }
-    @JDIAction(value = "Check that '{name}' is hidden", timeout = 0)
+    @JDIAction(value = "Check that '{name}' doesn't exist", timeout = 0)
     public boolean isNotExist() {
         return !isExist();
     }
@@ -426,7 +426,7 @@ public class UIElement extends JDIBase
      * Get all elements classes as list.
      * Example: <div class="btn submit color"> </a> will return list of elements [btn, submit, color]
      */
-    @JDIAction(value = "Execute javascript '{0}' for '{name}'", level = DEBUG)
+    @JDIAction(value = "Get classes of '{name}'", level = DEBUG)
     public List<String> classes() {
         String cl = attr("class");
         return cl.length() > 0
