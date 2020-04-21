4d406e646f144f30770a2e6e03bc9984120973d8
MF <maria_fedorova1@epam.com>
2020-04-21 16:22:40 +0300

    cherypick result - green

--- jdi-light-html-tests/src/test/java/io/github/epam/TestsInit.java
+++ jdi-light-html-tests/src/test/java/io/github/epam/TestsInit.java
@@ -16,6 +16,7 @@ import static com.epam.jdi.light.settings.WebSettings.logger;
 import static io.github.com.StaticSite.homePage;
 
 public interface TestsInit {
+
     @BeforeSuite(alwaysRun = true)
     default void setUp() {
         WebSettings.init();
